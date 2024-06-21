47f9b3cef07917d6961dc1519b0d46538840480c
Nevada Kingi-Whata <kingiwhata@gmail.com>
2024-06-21 18:15:09 +1200

    calendar setup

--- src/app/components/Calendar.tsx
+++ src/app/components/Calendar.tsx
@@ -5,15 +5,17 @@ export default function Calendar() {
     const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
     return (
-        <section className="bg-yellow-50 h-[390px] outline outline-4 mx-2 border-black rounded-[16px]">
-            <div className="flex justify-evenly h-7 border-black border-b">
-                {daysOfWeek.map((day, i) => (
-                    <div>{day}</div>
+        <section className="h-[390px] outline outline-4 mx-2 border-black rounded-[16px]">
+            <div className="flex justify-between h-7 border-black border-b">
+                {daysOfWeek.map((day, _) => (
+                    <div className="flex-1">{day}</div>
                 ))}
             </div>
-            <div className="grid grid-cols-7 grid-rows-6">
-                {getDates(6, 2024).map((date, i) => (
-                    <div>{date.getDate()}</div>
+            <div className="grid grid-cols-7 grid-rows-6 h-[calc(100%-1.75rem)]  [&>*:nth-child(7n)]:border-r-[0] [&>*:nth-last-child(-n+7)]:border-b-[0]">
+                {getDates(6, 2024).map((date, _) => (
+                    <div className="text-sm border-b border-r  border-black ">
+                        {date.getDate()}
+                    </div>
                 ))}
             </div>
         </section>
