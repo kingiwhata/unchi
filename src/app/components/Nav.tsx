0970c7df98ec02af3cb14dc31b9eb60842b6e27e
Nevada Kingi-Whata <kingiwhata@gmail.com>
2024-06-24 19:52:38 +1200

    back and forward date

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -10,20 +10,36 @@ export default function Nav({
 }) {
     return (
         <section className="flex flex-row justify-between bg-white h-20 w-full px-2 pt-1">
-            <div
-                className="flex flex-col justify-center"
-                onClick={() =>
-                    handleClick(
-                        getPreviousMonth(
-                            date.getMonth() - 1,
-                            date.getFullYear(),
-                        ),
-                    )
-                }
-            >
+            <div className="flex flex-row justify-center">
+                <p
+                    className="cursor-pointer"
+                    onClick={() =>
+                        handleClick(
+                            getPreviousMonth(
+                                date.getMonth() - 1,
+                                date.getFullYear(),
+                            ),
+                        )
+                    }
+                >
+                    {'<'}
+                </p>
                 <h1 className="text-3xl text-black">
                     {date.toLocaleString('default', { month: 'long' })}
                 </h1>
+                <p
+                    className="cursor-pointer"
+                    onClick={() =>
+                        handleClick(
+                            getPreviousMonth(
+                                date.getMonth() + 1,
+                                date.getFullYear(),
+                            ),
+                        )
+                    }
+                >
+                    {'>'}
+                </p>
             </div>
             <div className="flex flex-row w-fit">
                 <img src="unchicat.png" className="w-20 h-20" />
