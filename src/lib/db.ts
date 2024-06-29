2195bcef572a32df4e17c11ee16e5d1138cda36e
Nevada Kingi-Whata <kingiwhata@gmail.com>
2024-06-29 18:38:08 +1200

    creating table fix

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,13 +2,12 @@
 
 import { sql } from '@vercel/postgres';
 
-export default async function Handler() {
+export default async function InsertLog() {
     const createTable = await sql`CREATE TABLE IF NOT EXISTS Log (
-        id SERIAL PRIMARY KEY
-        stickers TEXT []
-        notes TEXT []
-        date_time TIMESTAMP
+        id SERIAL PRIMARY KEY,
+        stickers TEXT [],
+        notes TEXT [],
+        date_time TIMESTAMP,
         created_at TIMESTAMP
-        
-)`;
+    )`;
 }
