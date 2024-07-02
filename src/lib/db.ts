a2524b045fa9085ef4dea5a99743626bb2020a8b
Nevada Kingi-Whata <kingiwhata@gmail.com>
2024-07-03 00:08:20 +1200

    set uuid cookie and db

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,14 +1,20 @@
 //@ts-nocheck
 /* eslint no-use-before-define: 0 */ // --> OFF
 'use server';
-import { randomUUID } from 'crypto';
+import { UUID, randomUUID } from 'crypto';
 import { sql } from '@vercel/postgres';
+import { cookies } from 'next/headers';
 
 export default async function insertLog(
     stickers: string[],
     formData: FormData,
 ) {
-    const user_id = randomUUID();
+    let user_id = randomUUID();
+    if (cookies().has('uuid')) {
+        user_id = cookies().get('uuid').value;
+    } else {
+        cookies().set('uuid', user_id);
+    }
     const createTable = await sql`CREATE TABLE IF NOT EXISTS Log (
         id SERIAL PRIMARY KEY,
         stickers TEXT [],
