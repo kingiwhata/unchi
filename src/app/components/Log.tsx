9eddf17c383caa5866173dd9abf8842d46060143
Nevada Kingi-Whata <kingiwhata@gmail.com>
2024-07-08 22:25:44 +1200

    new log component done

--- src/app/components/Log.tsx
+++ src/app/components/Log.tsx
@@ -1,9 +1,10 @@
 'use client';
 import { Log } from '@/types/Log';
 import { NewLog } from './NewLog';
+import { checkIfLoggedDate } from '@/utils/dates';
 
 export default function UserLog({ date, logs }: { date: Date; logs: Log[] }) {
-    if (!logs) {
+    if (!checkIfLoggedDate(logs, date)) {
         return (
             <section className="flex flex-col justify-center items-center flex-1 m-2 bg-[url('/bg-unchi.jpg')] rounded-[16px]">
                 <NewLog date={date} />
--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -1,3 +1,5 @@
+import { Log } from '@/types/Log';
+
 export function getDates(month: number, year: number) {
     const firstOfMonth = new Date(year, month - 1, 1);
     firstOfMonth.setDate(firstOfMonth.getDate() - firstOfMonth.getDay());
@@ -12,4 +14,10 @@ export function getPreviousMonth(month: number, year: number) {
     const firstOfMonth = new Date(year, month, 1);
     firstOfMonth.setDate(firstOfMonth.getDate());
     return firstOfMonth;
+}
+
+export function checkIfLoggedDate(logArray: Log[], dateToCheck: Date) {
+    return logArray.some(
+        (log) => log.date_time.toDateString() === dateToCheck.toDateString(),
+    );
 }
