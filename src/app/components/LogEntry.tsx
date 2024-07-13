d8d82b657b4d2e1deee43f91a91da1bf07a29c1c
Nevada Kingi-Whata <kingiwhata@gmail.com>
2024-07-13 23:34:21 +1200

    new log and remove link

--- src/app/components/LogEntry.tsx
+++ src/app/components/LogEntry.tsx
@@ -2,22 +2,29 @@
 import LogType from '@/types/Log';
 import { useDataContext } from '@/utils/Provider';
 import Link from 'next/link';
+import { useState } from 'react';
+import NewLog from './NewLog';
 
 export default function LogEntry({ log }: { log: LogType }) {
     const { selectedDate } = useDataContext();
     const loggedTime = log.date_time.toTimeString().substring(0, 8);
+    const [showNew, setShowNew] = useState(false);
 
     const dateHref = selectedDate.toDateString().split(' ').join('-');
     return (
-        <div>
-            <div className="w-full flex justify-center border-black border rounded-2xl py-4">
-                <p>{log.sticker}</p>
-                <h2>{loggedTime}</h2>
-                {log.notes && <p>{log.notes}</p>}
-            </div>
-            <Link href={`new/${dateHref}`}>
-                <div>Add new log</div>
-            </Link>
+        <div className="w-full">
+            {!showNew && (
+                <>
+                    <div className="w-full flex justify-center border-black border rounded-2xl py-4">
+                        <p>{log.sticker}</p>
+                        <h2>{loggedTime}</h2>
+                        {log.notes && <p>{log.notes}</p>}
+                    </div>
+                    <div onClick={() => setShowNew(true)}>Add new log</div>
+                </>
+            )}
+
+            {showNew && <NewLog date={selectedDate} />}
         </div>
     );
 }
