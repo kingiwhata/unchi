2a0fdb4ab678f5544625bd126c3f90aba2e13c55
Nevada Kingi-Whata <kingiwhata@gmail.com>
2024-07-16 23:15:18 +1200

    adding date route for parallel calendar

--- src/app/components/Calendar.tsx
+++ src/app/components/Calendar.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { getDates } from '@/utils/dates';
 import Log from '@/types/Log';
+import Link from 'next/link';
 
 export default function Calendar({
     date,
@@ -14,40 +15,46 @@ export default function Calendar({
     const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
     return (
-        <section className="h-[390px] outline outline-4 mx-2 mb-2 border-black rounded-[16px]">
-            <div className="flex justify-between h-7 border-black border-b bg-white">
-                {daysOfWeek.map((day: string, i: number) => (
-                    <div key={i} className="flex justify-center flex-1">
-                        {day}
-                    </div>
-                ))}
-            </div>
-            <div className="grid grid-cols-7 grid-rows-6 h-[calc(100%-1.75rem)]  [&>*:nth-child(7n)]:border-r-[0] [&>*:nth-last-child(-n+7)]:border-b-[0]">
-                {getDates(date.getMonth() + 1, date.getFullYear()).map(
-                    (date: Date, i: number) => {
-                        let firstSticker = '';
-                        for (const log of logs) {
-                            if (
-                                log.date_time.toDateString() ===
-                                date.toDateString()
-                            ) {
-                                firstSticker = log.sticker;
-                                break;
+        <Link href={date.toDateString().split(' ').join('-')}>
+            <section className="h-[390px] outline outline-4 mx-2 mb-2 border-black rounded-[16px]">
+                <div className="flex justify-between h-7 border-black border-b bg-white">
+                    {daysOfWeek.map((day: string, i: number) => (
+                        <div key={i} className="flex justify-center flex-1">
+                            {day}
+                        </div>
+                    ))}
+                </div>
+                <div className="grid grid-cols-7 grid-rows-6 h-[calc(100%-1.75rem)]  [&>*:nth-child(7n)]:border-r-[0] [&>*:nth-last-child(-n+7)]:border-b-[0]">
+                    {getDates(date.getMonth() + 1, date.getFullYear()).map(
+                        (date: Date, i: number) => {
+                            let firstSticker = '';
+                            for (const log of logs) {
+                                if (
+                                    log.date_time.toDateString() ===
+                                    date.toDateString()
+                                ) {
+                                    firstSticker = log.sticker;
+                                    break;
+                                }
                             }
-                        }
-                        return (
-                            <div
-                                key={i}
-                                className="text-sm border-b border-r cursor-pointer border-black "
-                                onClick={() => handleSelected(date)}
-                            >
-                                <p>{date.getDate()}</p>
-                                <p className="text-center">{firstSticker}</p>
-                            </div>
-                        );
-                    },
-                )}
-            </div>
-        </section>
+                            return (
+                                <div
+                                    key={i}
+                                    className="text-sm border-b border-r cursor-pointer border-black "
+                                    onClick={() => {
+                                        handleSelected(date);
+                                    }}
+                                >
+                                    <p>{date.getDate()}</p>
+                                    <p className="text-center">
+                                        {firstSticker}
+                                    </p>
+                                </div>
+                            );
+                        },
+                    )}
+                </div>
+            </section>
+        </Link>
     );
 }
